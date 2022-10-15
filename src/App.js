3d4ce409f05fe2a53bc52f3863d8e133a47e4dd3
priliaaprilia08 <apriliapriliarahmawatia@gmail.com>
2022-10-15 22:16:33 +0700

    tugas deployment

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Selamat <code>Datang</code> di Website Static Aprilia.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Aprilia
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
